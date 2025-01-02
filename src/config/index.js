02816cca208c550c90c03637e4fe24c2f1b718e9
udemy <lkjh8sda@naver.com>
2025-01-03 08:08:21 +0900

    baseUrl change

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const environmentUrls = new Map();
 
 environmentUrls.set('localhost','http://localhost:8080');
-environmentUrls.set('book-store-web.herokuapp.com','https://book-store-web.herokuapp.com');
+environmentUrls.set('book-store-web-d8c8362782af.herokuapp.com','https://book-demo-store1-1c18ca138b3c.herokuapp.com');
 
 export default environmentUrls.get(window.location.hostname);
