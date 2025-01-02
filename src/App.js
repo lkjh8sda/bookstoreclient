ee7d6b04c0326be26a9466059fe0291dd7079f5b
udemy <lkjh8sda@naver.com>
2025-01-03 07:34:41 +0900

    setup env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-// import baseUrl from './config';
+import baseUrl from './config';
+import axios from 'axios';
 
 function App() {
 
+  axios(`${baseUrl}/api/v1/books`).then(books => {
+    console.log(books);
+  })
 
   return (
     <div className="App">
